cae14819867d59ec8c204c449865f2a0cbde0f3d
taylorbecker0524 <taylorbecker0524@gmail.com>
2026-05-11 14:33:57 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -27,7 +27,11 @@ export function createServerSupabase() {
   return createClient(url, key, { auth: { persistSession: false } })
 }
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+export function createBrowserSupabase() {
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+  if (!url || !key) {
+    throw new Error(`Missing Supabase env vars`)
+  }
+  return createClient(url, key)
+}
